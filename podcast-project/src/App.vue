e54d179268952b30e61cdb31fba20ccfcc912558
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-05-27 23:30:14 +0200

    Home is now the page, App is the App itself

--- podcast-project/src/App.vue
+++ podcast-project/src/App.vue
@@ -1,30 +1,14 @@
-// Vue file (component) = HTML (template) + JavaScript (script) + CSS (style)
-
 <template>
   <div id="app">
-    <Background/>
-    <h1>{{ title }}</h1>
-    <SearchBar/>
-    <Categories/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Background from './components/Background.vue'
-import SearchBar from './components/SearchBar.vue'
-import Categories from './components/Categories.vue'
-
 export default {
   name: 'App',
-  components: {Background, SearchBar, Categories},
-  data() {
-    return {
-      title: 'Vue Podcast App'
-    }
-  }
 }
 </script>
-
 
 <style>
 #app {
@@ -32,9 +16,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  /*color: #2c3e50;*/
   color: #fdfdfd;
   margin-top: 60px;
 }
 </style>
-
---
+++ podcast-project/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <Background/>
+      <h1>{{ title }} </h1>
+      <SearchBar/>
+      <Categories/>
+    </div>
+  </template>
+  
+<script>
+  import Background from './Background.vue'
+  import SearchBar from './SearchBar.vue'
+  import Categories from './Categories.vue'
+  
+export default {
+    name: 'Home',
+    components: { Background, SearchBar, Categories },
+    data() {
+      return {
+        title: 'Vue Podcast App'
+      }
+    }
+  }
+  </script>
+  
+<style>
+</style>
+  
