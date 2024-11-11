d5cec87967be6cbc122eac89e6738fe4680eeb9f
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-11-11 23:13:13 +0100

    fix: add body parser path

--- server-backend/index.js
+++ server-backend/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
