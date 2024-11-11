9e08d2e8d6995bd072af6d884d482e39b3594c4d
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-11-11 17:52:32 +0100

    fixing vuetify not showign

--- web-frontend/src/main.ts
+++ web-frontend/src/main.ts
@@ -4,10 +4,13 @@ import App from './App.vue'
 // Router
 import router from './router'
 
-// Vuetify
+// @ts-ignore
 import 'vuetify/styles'
+// @ts-ignore
 import { createVuetify } from 'vuetify'
+// @ts-ignore
 import * as components from 'vuetify/components'
+// @ts-ignore
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
@@ -16,10 +19,10 @@ const vuetify = createVuetify({
 })
 
 // VueDraggable for drag and drop
-import { VueDraggableNext } from 'vue-draggable-next';
+import { VueDraggableNext } from 'vue-draggable-next'
 
-const app = createApp(App);
-app.use(router);
-app.use(vuetify);
-app.component('draggable', VueDraggableNext);  // Register component globally
-app.mount('#app');
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.component('draggable', VueDraggableNext)  // Register component globally
+app.mount('#app')
