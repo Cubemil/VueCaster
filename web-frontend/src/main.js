87ee0ef9aa7fb806144dd68a02a9a1ceae414f10
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-11-17 13:22:07 +0100

    feat: remove Vuetify dependency and related code from the project

--- web-frontend/src/main.js
+++ web-frontend/src/main.js
@@ -4,21 +4,10 @@ import App from './App.vue'
 // Router
 import router from './router'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
 // VueDraggable for drag and drop
 import { VueDraggableNext } from 'vue-draggable-next'
 
 const app = createApp(App)
 app.use(router)
-app.use(vuetify)
 app.component('draggable', VueDraggableNext)  // Register component globally
 app.mount('#app')
