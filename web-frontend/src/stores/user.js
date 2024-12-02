91016fe54ffd276238004dcaaa36dff0986c1d94
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-12-02 12:25:35 +0100

    refactor: user store wrong variable and possible inconsistent checks

--- web-frontend/src/stores/user.js
+++ web-frontend/src/stores/user.js
@@ -4,36 +4,33 @@ export const useUserStore = defineStore("user", {
   state: () => {
     // on startup also check session storage
     const storedUsername = localStorage.getItem("username") || sessionStorage.getItem("username")
-    const token = localStorage.getItem("token") || sessionStorage.getItem("token")
+    const storedToken = localStorage.getItem("token") || sessionStorage.getItem("token")
 
     return {
-      username: storedUsername ? storedUsername : null,
-      loggedIn: token ? true : false,
-      token: token || null
+      username: storedUsername || null,
+      token: storedToken || null
     }
   },
   getters: {
     getUsername: (state) => state.username,
-    isLoggedIn: (state) => state.loggedIn,
+    isLoggedIn: (state) => !!state.token,
   },
   actions: {
     login(username, token, rememberMe) {
+      if (!username || !token) {
+        throw new Error("Invalid login credentials.")
+      }
+
       this.username = username
-      this.loggedIn = true
       this.token = token
       
       // store token only in session when user doesn't want their credentials to be remembered
-      if (rememberMe) {
-        localStorage.setItem("token", token)
-        localStorage.setItem("username", username)
-      } else {
-        sessionStorage.setItem("token", token)
-        sessionStorage.setItem("username", username)
-      }
+      const storage = rememberMe ? localStorage : sessionStorage
+      storage.setItem("token", token)
+      storage.setItem("username", username)
     },
     logout() {
-      this.user = null
-      this.loggedIn = false
+      this.username = null
       this.token = null
 
       // clear from all storages
