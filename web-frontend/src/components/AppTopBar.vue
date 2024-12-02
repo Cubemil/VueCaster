abce4833132bb1618504025007ff49d2d461bf56
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-12-02 12:09:42 +0100

    feat: update AppTopBar to display welcome message and redirect based on user login status

--- web-frontend/src/components/AppTopBar.vue
+++ web-frontend/src/components/AppTopBar.vue
@@ -4,14 +4,30 @@
 	  <i class="fas fa-ellipsis-h"></i>
   </div>
   <div id="right-section">
-    <router-link id="router-link-login" to="login" class="router-link">
+		<span id="log-in-text" v-if="userStore.username">Welcome, {{ userStore.username }}!</span>
+		<span id="log-in-text" v-else>Log in to use all features</span>
+    <router-link :to="redirectPath" class="router-link">
       <i class="fas fa-user-circle"></i>
     </router-link>
 	</div>
 </div>
 </template>
 
+<script setup>
+import { useUserStore } from '@/stores/user'
+</script>
+
 <script>
+export default {
+	computed: {
+		userStore() {
+			return useUserStore()
+		},
+		redirectPath() {
+			return this.userStore.username ? '/profile' : '/login'
+		}
+	}
+}
 </script>
 
 <style scoped>
@@ -36,10 +52,11 @@
   text-decoration: none;
   color: #B3B3B3;
   transition: all 0.3s;
+	transform: scale(1.1);
+	color: #ffffff;
 }
 
-#router-link-login:hover {
-  transform: scale(1.1);
-  color: #ffffff;
+#log-in-text {
+	margin-right: 1%;
 }
 </style>
