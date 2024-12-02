3d9fe182c3926430ab6dcc437082ec35eb4d7039
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-12-02 12:08:22 +0100

    feat: add webpack DefinePlugin for hydration mismatch details in vue.config.js

--- web-frontend/vue.config.js
+++ web-frontend/vue.config.js
@@ -1,15 +1,23 @@
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
 
 module.exports = defineConfig({
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false)
+      })
+    ]
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
         }
+      }
     }
-});
+  }
+})
