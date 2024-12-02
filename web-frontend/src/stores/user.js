1fa3f361026a3ec06540d110f8815c3e52d25c75
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-12-02 12:09:17 +0100

    feat: enhance user store to manage username and token with session/local storage

--- web-frontend/src/stores/user.js
+++ web-frontend/src/stores/user.js
@@ -1,22 +1,46 @@
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore("user", {
-  state: () => ({
-    user: null,
-    loggedIn: false,
-  }),
+  state: () => {
+    // on startup also check session storage
+    const storedUsername = localStorage.getItem("username") || sessionStorage.getItem("username")
+    const token = localStorage.getItem("token") || sessionStorage.getItem("token")
+
+    return {
+      username: storedUsername ? storedUsername : null,
+      loggedIn: token ? true : false,
+      token: token || null
+    }
+  },
   getters: {
+    getUsername: (state) => state.username,
     isLoggedIn: (state) => state.loggedIn,
-    getUserData: (state) => state.user,
   },
   actions: {
-    login(user) {
-      this.user = user
+    login(username, token, rememberMe) {
+      this.username = username
       this.loggedIn = true
+      this.token = token
+      
+      // store token only in session when user doesn't want their credentials to be remembered
+      if (rememberMe) {
+        localStorage.setItem("token", token)
+        localStorage.setItem("username", username)
+      } else {
+        sessionStorage.setItem("token", token)
+        sessionStorage.setItem("username", username)
+      }
     },
     logout() {
       this.user = null
       this.loggedIn = false
+      this.token = null
+
+      // clear from all storages
+      localStorage.removeItem("token")
+      localStorage.removeItem("username")
+      sessionStorage.removeItem("token")
+      sessionStorage.removeItem("username")
     }
   }
 })
